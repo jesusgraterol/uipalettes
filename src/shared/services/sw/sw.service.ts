ea1e701d15c0a17d89d5387b10d0149a91606e58
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-05-24 11:36:44 -0400

    Complete the initial implementation of the Service Worker and the installation flow
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/shared/services/sw/sw.service.ts
+++ src/shared/services/sw/sw.service.ts
@@ -1,5 +1,5 @@
 import { encodeError, extractMessage } from 'error-message-utils';
-import { ISWService } from './types';
+import { IInstallerPromptOutcome, IBeforeInstallPromptEvent, ISWService } from './types';
 import { ERRORS } from '../../errors';
 
 /* ************************************************************************************************
@@ -14,11 +14,88 @@ const SWServiceFactory = (): ISWService => {
   let __worker: ServiceWorker | undefined;
   let __registrationError: string | undefined;
 
+  // app installer
+  let __installer: IBeforeInstallPromptEvent | undefined;
+  let __installerPromptOutcome: IInstallerPromptOutcome | undefined;
+  let __appInstalled: boolean | undefined;
+  const __runningInstalledApp: boolean = window.matchMedia('(display-mode: standalone)').matches;
+
+
 
 
 
   /* **********************************************************************************************
-   *                                         REGISTRATION                                         *
+   *                                        APP INSTALLER                                         *
+   ********************************************************************************************** */
+
+  /**
+   * Subscribes to the appinstalled which is fired by Chromium-based browsers and indicate when the
+   * app has completed installing on the user's device. For more info visit:
+   * https://web.dev/learn/pwa/detection
+   */
+  const __subscribeToInstallationState = () => {
+    window.addEventListener(
+      'appinstalled',
+      () => {
+        __appInstalled = true;
+      },
+      { once: true },
+    );
+  };
+
+  /**
+   * Triggers when the service worker is registered. It subscribes to the beforeinstallprompt which
+   * is issued by browsers that support PWAs and provde the ability to trigger the browser's install
+   * prompt. For more info visit:
+   * https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/How_to/Trigger_install_prompt
+   * https://web.dev/learn/pwa/installation-prompt
+   * https://stackoverflow.com/questions/50332119/is-it-possible-to-make-an-in-app-button-that-triggers-the-pwa-add-to-home-scree
+   */
+  const __initializeAppInstaller = () => {
+    window.addEventListener(
+      'beforeinstallprompt',
+      (e: IBeforeInstallPromptEvent) => {
+        // prevent the mini-infobar from appearing on mobile
+        e.preventDefault();
+
+        // store the event
+        __installer = e;
+
+        // listen to the installation state
+        __subscribeToInstallationState();
+      },
+      { once: true },
+    );
+  };
+
+  /**
+   * Attempts to install the application on the user's device and stores the prompt's outcome.
+   * @returns Promise<void>
+   * @throws
+   * - NO_APP_INSTALLER: if the beforeinstallprompt did not trigger
+   *
+   * More info:
+   * https://web.dev/learn/pwa/installation-prompt
+   */
+  const installApp = async (): Promise<void> => {
+    if (__installer && typeof __installer.prompt === 'function') {
+      __installer.prompt();
+      const { outcome } = await __installer.userChoice;
+      __installerPromptOutcome = outcome;
+
+      // the installer prompt can only be used once
+      __installer = undefined;
+    } else {
+      throw new Error(encodeError('The app cannot be installed because the prompt event was not provided by the browser.', ERRORS.NO_APP_INSTALLER));
+    }
+  };
+
+
+
+
+
+  /* **********************************************************************************************
+   *                                 SERVICE WORKER REGISTRATION                                  *
    ********************************************************************************************** */
 
   /**
@@ -47,8 +124,8 @@ const SWServiceFactory = (): ISWService => {
               __worker = <ServiceWorker>e.target;
             });
 
-            // store the app installer
-            // ...
+            // init the app's installer
+            __initializeAppInstaller();
           } else {
             console.log(registration);
             __registrationError = encodeError('The Service Worker\'s Registration is empty', ERRORS.EMPTY_SW_REGISTRATION);
@@ -66,6 +143,7 @@ const SWServiceFactory = (): ISWService => {
 
 
 
+
   /* **********************************************************************************************
    *                                         MODULE BUILD                                         *
    ********************************************************************************************** */
@@ -77,8 +155,20 @@ const SWServiceFactory = (): ISWService => {
     get registrationError() {
       return __registrationError;
     },
+    get installerPromptOutcome() {
+      return __installerPromptOutcome;
+    },
+    get appInstalled() {
+      return __appInstalled;
+    },
+    get runningInstalledApp() {
+      return __runningInstalledApp;
+    },
 
-    // registration
+    // app installer
+    installApp,
+
+    // service worker registration
     register,
   });
 };
--- src/shared/services/sw/types.ts
+++ src/shared/services/sw/types.ts
@@ -5,16 +5,65 @@
  ************************************************************************************************ */
 
 /**
+ * Installer Prompt Outcome
+ * The action taken by the user once the installation prompt is displayed.
+ */
+type IInstallerPromptOutcome = 'accepted' | 'dismissed';
+
+/**
+ * The BeforeInstallPromptEvent is fired at the Window.onbeforeinstallprompt handler
+ * before a user is prompted to "install" a web site to a home screen on mobile.
+ * @important supported on Chrome and Android Webview.
+ *
+ * More Info:
+ * https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeinstallprompt_event
+ * https://developer.mozilla.org/en-US/docs/Web/API/BeforeInstallPromptEvent
+ */
+interface IBeforeInstallPromptEvent extends Event {
+  // returns an array of DOMString items containing the platforms on which the event was dispatched.
+  // This is provided for user agents that want to present a choice of versions to the user such as,
+  // for example, "web" or "play" which would allow the user to chose between a web version or
+  // an Android version.
+  readonly platforms: Array<string>;
+
+  // returns a Promise that resolves to a DOMString containing either "accepted" or "dismissed".
+  readonly userChoice: Promise<{
+    outcome: IInstallerPromptOutcome,
+    platform: string
+  }>;
+
+  // allows a developer to show the install prompt at a time of their own choosing.
+  prompt(): Promise<void>;
+}
+
+/**
+ * Window Events
+ * This technology is highly experimental and therefore, not all browser vedors have a "stable"
+ * approach.
+ */
+declare global {
+  interface WindowEventMap {
+    beforeinstallprompt: IBeforeInstallPromptEvent;
+  }
+}
+
+/**
  * SW Service
  * The singleton that will handle the Service Worker's state as well as its functionality.
  */
 interface ISWService {
   // properties
-  worker: ServiceWorker | undefined,
-  registrationError: string | undefined,
+  worker: ServiceWorker | undefined;
+  registrationError: string | undefined;
+  installerPromptOutcome: IInstallerPromptOutcome | undefined;
+  appInstalled: boolean | undefined;
+  runningInstalledApp: boolean;
 
-  // registration
-  register(path?: string): void
+  // app installer
+  installApp(): Promise<void>;
+
+  // service worker registration
+  register(path?: string): void;
 }
 
 
@@ -24,5 +73,7 @@ interface ISWService {
  *                                         MODULE EXPORTS                                         *
  ************************************************************************************************ */
 export type {
+  IInstallerPromptOutcome,
+  IBeforeInstallPromptEvent,
   ISWService,
 };
