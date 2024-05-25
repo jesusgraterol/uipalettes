6c6e2ef9da126316fe741400d3df08c7c75eb3cc
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-05-25 14:54:55 -0400

    Include MD's palettes
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/components/palettes/clipboard-card.component.tsx
+++ src/components/palettes/clipboard-card.component.tsx
@@ -26,7 +26,6 @@ const ClipboardCard = ({ hex }: IClipboardCardProps) => {
    ********************************************************************************************** */
 
   useEffect(() => {
-    setActiveHex(undefined);
     let outerTimeout: number;
     let innerTimeout: number;
     if (typeof hex === 'string') {
@@ -51,7 +50,7 @@ const ClipboardCard = ({ hex }: IClipboardCardProps) => {
    ********************************************************************************************** */
   return (
     <div role='tooltip'
-        className={`fixed top-5 inset-x-0 z-20 w-64 md:w-72 mx-auto text-left bg-white shadow-6 transition-transform ${activeHex ? 'translate-y-0' : '-translate-y-32'}`}>
+        className={`fixed top-5 inset-x-0 z-20 w-64 md:w-72 mx-auto text-left bg-white shadow-6 transition-transform duration-500 ${activeHex ? 'translate-y-0' : '-translate-y-32'}`}>
       <div className='flex justify-start items-center p-3'>
         <div className='w-11 h-11 rounded-md' style={{ backgroundColor: activeHex }}></div>
         <div className='ml-2'>
